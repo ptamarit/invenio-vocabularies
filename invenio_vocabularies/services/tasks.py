0b09f8639754d1fcae0fa7ac2e43d6785d8b9386
Pablo Tamarit <pablo.tamarit@cern.ch>
2024-05-27 17:45:12 +0200

    datastreams: funders celery task

--- invenio_vocabularies/services/tasks.py
+++ invenio_vocabularies/services/tasks.py
@@ -11,11 +11,33 @@ from celery import shared_task
 from flask import current_app
 
 from ..datastreams.factories import DataStreamFactory
+from ..factories import get_vocabulary_config
 
 
 @shared_task(ignore_result=True)
 def process_datastream(config):
     """Process a datastream from config."""
+    ds = DataStreamFactory.create(
+        readers_config=config["readers"],
+        transformers_config=config.get("transformers"),
+        writers_config=config["writers"],
+    )
+
+    for result in ds.process():
+        if result.errors:
+            for err in result.errors:
+                current_app.logger.error(err)
+
+
+@shared_task(bind=True)
+def process_funders(self):
+    """Process the funders datastream."""
+    vc = get_vocabulary_config("funders")
+    config = vc.get_config()
+
+    for w_conf in config["writers"]:
+        w_conf["args"]["update"] = True
+
     ds = DataStreamFactory.create(
         readers_config=config["readers"],
         transformers_config=config.get("transformers"),
